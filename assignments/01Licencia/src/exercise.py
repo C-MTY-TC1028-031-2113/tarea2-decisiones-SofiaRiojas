ac37ff4858aabef18386e7b6a50c5fa4687aa361
A01177990 <a01177990@itesm.mx>
2021-08-27 09:58:54 -0500

    27/08/2021

--- assignments/01Licencia/src/exercise.py
+++ assignments/01Licencia/src/exercise.py
@@ -1,6 +1,16 @@
 
 def main():
     pass
+edad =  int(input("Ingresa tu edad:"))
+documento = str(input("¿Tienes identificación oficial?(s/n):"))
+if edad >=18 and documento == "s":
+    print ("Trámite de licencia concedido")
+elif (edad >=18 or edad >0<18) and documento == "n":
+    print ("No cumples requisitos")
+elif documento != ("s" or "n") or edad < 0:
+    print ("Respuesta incorrecta")
+else:
+    print ("No cumples requisitos")
 
 
 if __name__ == '__main__':
