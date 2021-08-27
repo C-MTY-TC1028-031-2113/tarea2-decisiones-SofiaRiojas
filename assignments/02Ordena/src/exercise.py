e8be670049ba485d76195b1ecf9c7312b42922d0
A01177990 <a01177990@itesm.mx>
2021-08-27 18:11:58 -0500

    27082021

--- assignments/02Ordena/src/exercise.py
+++ assignments/02Ordena/src/exercise.py
@@ -3,6 +3,32 @@ def main():
     x = int(input("Ingresa el primer número: "))
     y = int(input("Ingresa el segundo número: "))
     z = int(input("Ingresa el tercer número: "))
+    if x<y and y<z: 
+        print (x)
+        print (y)
+        print (z)
+    elif x<z and z<y:
+        print (x)
+        print (z)
+        print (y)
+    elif y<x and x<z:
+        print (y)
+        print (x)
+        print (z)
+    elif y<z and z<x:
+        print (y)
+        print (z)
+        print (x)
+    elif z<x and x<y:
+        print (z)
+        print (x)
+        print (y) 
+    else:
+        print (z)
+        print (y)
+        print (x)
+
+
     
 
 
